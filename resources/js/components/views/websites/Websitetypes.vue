0794dc8f944d9a2de9f2103c7985b5320779926a
airat <airat@onlbp.ru>
2020-08-20 19:28:48 +0300

    fix websites

--- resources/js/components/views/websites/Websitetypes.vue
+++ resources/js/components/views/websites/Websitetypes.vue
@@ -62,7 +62,9 @@
                 class="font-weight-medium teal--text text--darken-3"
               >- верстка и программирование</span>.
               Верстка, это сборка макета и придание определенных стилей. Программирование, это когда задается функциональность. Необходимо понимать, что сайт состоит из двух основных частей -
-              <span class="font-weight-medium teal--text text--darken-3">фронтенд и бекенд</span>. Фронтенд - это видимая часть. Бекенд - невидимая, и находится по ту сторону сети, на сервере.
+              <span
+                class="font-weight-medium teal--text text--darken-3"
+              >фронтенд и бекенд</span>. Фронтенд - это видимая часть. Бекенд - невидимая, и находится по ту сторону сети, на сервере.
             </p>
             <p>
               Третье
@@ -70,31 +72,42 @@
                 class="font-weight-medium teal--text text--darken-3"
               >- верстка и программирование</span>.
               Верстка, это сборка макета и придание определенных стилей. Программирование, это когда задается функциональность. Необходимо понимать, что сайт состоит из двух основных частей -
-              <span class="font-weight-medium teal--text text--darken-3">фронтенд и бекенд</span>. Фронтенд - это видимая часть. Бекенд - невидимая, и находится по ту сторону сети, на сервере.
+              <span
+                class="font-weight-medium teal--text text--darken-3"
+              >фронтенд и бекенд</span>. Фронтенд - это видимая часть. Бекенд - невидимая, и находится по ту сторону сети, на сервере.
             </p>
             <p>
               Четвертое
               <span
                 class="font-weight-medium teal--text text--darken-3"
-              > - заполнение контентом. </span>.
-Сюда входят приобретение и обработка фотографий, аудио-видео материалов, написание статей и просто любых тектов.<span class="font-weight-medium teal--text text--darken-3">фронтенд и бекенд</span>. Фронтенд - это видимая часть. Бекенд - невидимая, и находится по ту сторону сети, на сервере.
+              >- заполнение контентом.</span>.
+              Сюда входят приобретение и обработка фотографий, аудио-видео материалов, написание статей и просто любых тектов. Фронтенд - это видимая часть. Бекенд - невидимая, и находится по ту сторону сети, на сервере.
             </p>
             <p>
               Пятое
               <span
                 class="font-weight-medium teal--text text--darken-3"
-              > - пуско-наладочные работы и конечное тестирование. </span>.
-Сайт выводится на сервер, приобретается доменное имя и привязывается к сайту. Происходит полное тестирование всего функционала, сверяется с техническим задаение. После этого этапа работа считается завершенной и передается клиенту.            </p>
+              >- пуско-наладочные работы и конечное тестирование.</span>.
+              Сайт выводится на сервер, приобретается доменное имя и привязывается к сайту. Происходит полное тестирование всего функционала, сверяется с техническим задаение. После этого этапа работа считается завершенной и передается клиенту.
+            </p>
           </v-card-text>
         </div>
-        <v-icon
-          size="150"
-          class="ma-10 ml-0 hidden-sm-and-down"
-          color="teal lighten-1"
-        >mdi-altimeter</v-icon>
-        <!-- <v-avatar class="ma-3" size="125" tile>
-            <v-img src="/assets/card-sm-bg.webp"></v-img>
-        </v-avatar>-->
+        <div class="col d-flex align-center">
+          <router-link to="/page" style="text-decoration:none">
+            <v-icon
+              v-show="!hinter3301"
+              size="150"
+              class="ma-10 ml-0 hidden-sm-and-down"
+              color="teal lighten-1"
+              link
+              to="/page"
+            >mdi-altimeter</v-icon>
+          </router-link>
+
+          <v-avatar class="ma-10 ml-0 hidden-sm-and-down" size="150" tile v-show="hinter3301">
+            <v-img src="/assets/3301.webp"></v-img>
+          </v-avatar>
+        </div>
       </div>
       <v-card-actions></v-card-actions>
     </v-card>
@@ -102,25 +115,21 @@
       <v-card-title class="green--text text--darken-3">Стоимость сайта</v-card-title>
       <div class="d-flex flex-no-wrap justify-space-between">
         <div>
-          <v-card-text
-            class="text-body-1"
-          >
-<p>
-              Стоимость сайта складывается из многих параметров. Но в основном, связаны с трудозатратами. 
+          <v-card-text class="text-body-1">
+            <p>
+              Стоимость сайта складывается из многих параметров. Но в основном, связаны с трудозатратами.
               <span
                 class="font-weight-medium teal--text text--darken-3"
-              >Трудозатраты</span>.
-- значит, время необходимое для реализации проекта. Но это еще не всё. Сайт можно сделать используя разные инструменты. Некоторые - платные, используя некоторые можно сделать быстро. Можно даже сделать два одинаково выглядящих сайта с большой разницей в стоимости. Они будут отличаться в легкости поддержки или изменения. Также, можно сделать или шаблонный сайт, или создавать уникальный дизайн или функционал. 
-            </p>          </v-card-text>
+              >Трудозатраты</span>
+              - значит, время необходимое для реализации проекта. Но это еще не всё. Сайт можно сделать используя разные инструменты. Некоторые - платные, используя некоторые можно сделать быстро. Можно даже сделать два одинаково выглядящих сайта с большой разницей в стоимости. Они будут отличаться в легкости поддержки или изменения. Также, можно сделать или шаблонный сайт, или создавать уникальный дизайн или функционал.
+            </p>
+          </v-card-text>
         </div>
         <v-icon
           size="150"
           class="ma-10 ml-0 hidden-sm-and-down"
           color="green lighten-2"
         >mdi-cash-usd-outline</v-icon>
-        <!-- <v-avatar class="ma-3" size="125" tile>
-            <v-img src="/assets/card-sm-bg.webp"></v-img>
-        </v-avatar>-->
       </div>
       <v-card-actions></v-card-actions>
     </v-card>
@@ -133,6 +142,23 @@ export default {
   computed: {
     ...mapGetters(["subheaderSize", "iconsSize"]),
   },
+  data: () => ({
+    hinter3301: false,
+  }),
   mounted() {},
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll(event) {
+      this.hinter3301 = !this.hinter3301;
+      setTimeout(() => {
+        this.hinter3301 = !this.hinter3301;
+      }, 10);
+    },
+  },
 };
 </script>
