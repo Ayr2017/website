636f170f79bfcf25717c3521da65533bc02c24f5
airat <airat@onlbp.ru>
2020-08-28 17:44:49 +0300

    yandex metrica

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Разработка сайтов, десктопных приложений и веб-анимации">
     <meta property="og:description" content="Разработка сайтов, десктопных приложений и веб-анимации">
     <meta name="yandex-verification" content="cf64e599556197ce" />
-    
+
     <link rel="icon" href="./assets/favicon.ico" type="image/x-icon">
 
 
@@ -15,7 +15,8 @@
 
     <!-- Fonts -->
 
-    <link rel="stylesheet"  href="{{ asset('css/app.css') }}?v=1.2&display=swap" media="none" onload="if(media!='all')media='all'">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=1.2&display=swap" media="none"
+        onload="if(media!='all')media='all'">
     <!-- <link href="https://cdn.jsdelivr.net/npm/animate.css@3.5.1" rel="stylesheet" type="text/css"> -->
 
     <!-- CSRF Token -->
@@ -23,6 +24,29 @@
     <!-- Scrollmagic -->
     <!-- <script async src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
     <script async src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script> -->
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode
+                .insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(66827806, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true
+        });
+
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/66827806" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 
 <body>
